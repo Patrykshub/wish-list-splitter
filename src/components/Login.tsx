9820edb8fd43d751fe9a96d3d1da589a558f2905
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-07-28 13:34:07 +0200

    feat: preparing the project for typescript

---
+++ src/components/Login.tsx
@@ -0,0 +1,13 @@
+function Login() {
+    return (
+    <ul>
+        <li>
+            <input></input>
+        </li>
+        <li>
+        <input></input>
+        </li>
+    </ul>
+    );
+};
+export default Login;
