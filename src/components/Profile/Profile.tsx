04bdf2803d024837a148090db72d380d17dd6f10
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-08-24 20:05:42 +0200

    feat: profile001

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,9 +1,46 @@
+import React, { useState } from "react";
 
-const Profile:React.FC<{}> = (props) => {
-    return (<div>
-        <div>
-            <p>Your Profile.</p>
+import classes from "./Profile.module.css";
+
+const Profile = (validateValue: any) => {
+  const [changingMode, setChangingMode] = useState(false);
+  const [editing, setEditing] = useState();
+  const [value, setValue] = useState();
+
+    const modeHandler = () => {
+        setChangingMode(true);
+    }
+
+  const nameHandler = (event: any) => {
+    setEditing(event.target.value);
+  };
+
+  const save = () => {
+    setValue(editing);
+    setChangingMode(false);
+  };
+
+  return (
+    <div className={classes.profile}>
+      <p >Your Profile</p>
+      <div>
+        <label className={classes.profile_user}>Name:</label>
+        <label> {value}</label>
         </div>
-    </div>)
-}
+        <div>
+        <label className={classes.profile_user}>Age:</label>
+        <label> {value}</label>
+
+        {!changingMode && <button onClick={modeHandler}>Edit</button>}
+        {changingMode && (
+          <>
+            <input onChange={nameHandler}></input>
+            <button onClick={save}>Ok</button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
 export default Profile;
